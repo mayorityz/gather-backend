81cbc45af4fba6b61e421af3bd44f5de17e644a2
Taiwo Oluwaseun <taiwoluwaseun8@gmail.com>
2022-04-19 15:33:03 +0100

    modified email

--- config/email.js
+++ config/email.js
@@ -1,13 +1,15 @@
 import dotenv from 'dotenv';
-import mailgun from 'mailgun-js';
+import formData from 'form-data';
+import Mailgun from 'mailgun.js';
 
 dotenv.config({ path: "./config.env" });
 
 const API_KEY = process.env.API_KEY;
 const DOMAIN = process.env.DOMAIN;
-const mg = mailgun ({ apiKey: API_KEY, domain: DOMAIN });
+const mailgun = new Mailgun(formData);
+const mg = mailgun.client({ username: 'api', key: API_KEY });
 
-const sendMail = async (sender_email, receiver_email, email_subject, email_body) => {
+const sendMail = (sender_email, receiver_email, email_subject, email_body) => {
     const data = {
         "from": sender_email,
         "to": receiver_email,
@@ -15,10 +17,9 @@ const sendMail = async (sender_email, receiver_email, email_subject, email_body)
         "text": email_body
     };
 
-    await mg.messages().send(data, (error, body) => {
-        if (error) console.log(error)
-        else console.log(body);
-    });
+    mg.messages.create(DOMAIN, data)
+       .then(msg => console.log(msg))
+       .catch(err => console.log(err));
 };
 
 export default sendMail;
---
+++ config/mailingList.js
@@ -0,0 +1,6 @@
+// import dotenv from 'dotenv';
+// import mailgun from 'mailgun-js';
+
+// dotenv.config({ path: "./config.env" });
+
+// const DOMAIN = process.env.DOMAIN;
