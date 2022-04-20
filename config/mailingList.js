2a85d937d8bd17234c5354f738ee8f520869c7be
Taiwo Oluwaseun <taiwoluwaseun8@gmail.com>
2022-04-20 23:53:13 +0100

    created mailing list, modified subscribers api and added subscriber

--- config/mailingList.js
+++ config/mailingList.js
@@ -1,6 +1,41 @@
-// import dotenv from 'dotenv';
-// import mailgun from 'mailgun-js';
+import dotenv from 'dotenv';
+import formData from 'form-data';
+import Mailgun from 'mailgun.js';
 
-// dotenv.config({ path: "./config.env" });
+dotenv.config({ path: "../config.env" });
 
-// const DOMAIN = process.env.DOMAIN;
+const API_KEY = process.env.API_KEY;
+const DOMAIN = process.env.DOMAIN;
+const MAILING_LIST_DOMAIN = process.env.MAILING_LIST_DOMAIN;
+const mailgun = new Mailgun(formData);
+const client = mailgun.client({ username: 'api', key: API_KEY });
+
+const createMailingList = async () => {
+    try {
+        const newList = await client.lists.create({
+            address: `wegatherdey@${DOMAIN}`,
+            name: "wegatherdey",
+            description: "list of subscribers"
+        });
+        console.log('newList', newList);
+    } catch (error) {
+        console.error(error);        
+    }
+};
+
+// createMailingList();
+
+export const addMember = async (email) => {
+    try {
+        const data = {
+            address: email,
+            subscribed: 'yes'
+        };
+    
+        const newMember = await client.lists.members.createMember(MAILING_LIST_DOMAIN, data);
+        
+        console.log('newMember', newMember);
+    } catch (error) {
+        console.error(error);
+    }
+};
--- controllers/subscriberController.js
+++ controllers/subscriberController.js
@@ -1,19 +1,9 @@
-import Subscriber from '../models/subscriber.js';
+import { addMember } from "../config/mailingList.js";
 
 export const subscribe = async (req, res) => {
     const { email } = req.body;
 
-    try {
-        const existingSubscriber = await Subscriber.findOne({ email });
-
-        if (existingSubscriber) return res.status(400).json({ message: "Subscriber already exists." });
-
-        const newSubscriber = await Subscriber.create({ email });
-
-        res.status(200).json(newSubscriber);
-    } catch (error) {
-        res.status(500).json({ message: 'Something went wrong.' });
-    }
+    await addMember(email);
 }
 
 export const unsubscribe = async (req, res) => {
