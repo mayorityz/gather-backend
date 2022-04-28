1afb7ee18c939fed35a4707a9c7b7bbe29bf458b
Taiwo Oluwaseun <taiwoluwaseun8@gmail.com>
2022-04-28 09:03:55 +0100

    product model

---
+++ models/product.js
@@ -0,0 +1,32 @@
+import mongoose from 'mongoose';
+
+const productSchema = mongoose.Schema({
+    artisan: {
+        type: String,
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    likes: {
+        type: [String],
+        default: []
+    },
+    comments: {
+        type: [String],
+        default: []
+    },
+    createdAt: {
+        type: Date,
+        default: new Date()
+    }
+});
+
+const Product = mongoose.model('product', productSchema);
+
+export default Product;
