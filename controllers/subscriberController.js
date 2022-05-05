3fb7f03c3f82c958cfc00cb993b1aa5b78d06e78
Taiwo Oluwaseun <taiwoluwaseun8@gmail.com>
2022-05-05 13:57:43 +0100

    modified subscriber controller and routes

--- controllers/subscriberController.js
+++ controllers/subscriberController.js
@@ -5,21 +5,3 @@ export const subscribe = async (req, res) => {
 
     await addMember(email);
 }
-
-export const unsubscribe = async (req, res) => {
-    const { email } = req.params;
-
-    await Subscriber.findOneAndRemove({ email });
-
-    res.json({ message: 'unsubscribe successful' });
-}
-
-export const getAllSubscribers = async (req, res) => {
-    try {
-        const subscribers = await Subscriber.find();
-
-        res.status(200).json({ subscribers });
-    } catch (error) {
-        res.status(500).json({ message: 'Something went wrong.' });
-    }
-}
--- routes/subscribersRoutes.js
+++ routes/subscribersRoutes.js
@@ -1,11 +1,9 @@
 import express from 'express';
 
-import { getAllSubscribers, subscribe, unsubscribe } from '../controllers/subscriberController.js';
+import { subscribe } from '../controllers/subscriberController.js';
 
 const router = express.Router();
 
 router.post('/subscribe', subscribe);
-router.delete('/unsubscribe/:email', unsubscribe);
-router.get('/', getAllSubscribers);
 
 export default router;
